6d4dd0fe20d908d5ce943ffc6aae29e2486a962e
lewis mcmillan <lewistmcmillan@gmail.com>
2023-08-17 11:22:47 +0100

    removed accidently added docs on main

--- docs/src/iarray.f90
+++
@@ -1,7 +0,0 @@
-module iarray
-!!  Contains all array variable names.
-
-    implicit none
-    !> stores the optical properties, i.e the total cross section \(\mu_t = \mu_s + \mu_a\)
-    real, allocatable :: rhokap(:,:,:)
-end module iarray
